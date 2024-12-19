f3ff35e85c7a849e5366c88562d3a5cafaca5e0c
Christian Landgren <christian@landgren.nu>
2024-12-19 13:27:04 +0100

    refactor: Simplify company service API calls and remove unused methods

--- src/services/api/companyService.ts
+++ src/services/api/companyService.ts
@@ -1,32 +1,11 @@
 import { GET } from './baseApi'
-import type { components } from './generated/api-types'
-
-type Company = components['schemas']['Company']
-type Property = components['schemas']['Property']
 
 export const companyService = {
   // Get all companies
   async getAll() {
-    const { data, error } = await GET('/companies', {})
+    const { data, error } = await GET('/companies')
     if (error) throw error
-    return data?.content as Company[]
+    console.log('companies', data)
+    return data?.content
   },
-
-  // Get company by ID
-  async getById(id: string) {
-    const { data, error } = await GET('/companies/{id}', {
-      params: { path: { id } }
-    })
-    if (error) throw error
-    return data?.content as Company
-  },
-
-  // Get properties for a company
-  async getCompanyProperties(companyId: string) {
-    const { data, error } = await GET('/companies/{companyId}/properties', {
-      params: { path: { companyId } }
-    })
-    if (error) throw error
-    return data?.content as Property[]
-  }
 }
